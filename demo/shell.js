5513379ca3167d8f22330819d27a41f21263056e
luozijian <lzj564774252@gmail.com>
2017-01-08 23:54:53 +0800

    refactor:upgrade code

--- demo/shell.js
+++ demo/shell.js
@@ -5,7 +5,7 @@ $("<iframe src="+window.location.href+" width='400px' height='400px' id='qunarFr
 $('#qunarFrame').get(0).onload = function () {
     var is_onload = false;
     setTimeout(test(this,is_onload),1000);
-    
+
 };
 function _test(context,is_onload) {
     return function () {
@@ -13,30 +13,83 @@ function _test(context,is_onload) {
     }
 }
 
+function randomNum(minNum,maxNum){
+    switch(arguments.length){
+        case 1:
+            return parseInt(Math.random()*minNum+1);
+            break;
+        case 2:
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum);
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+
 function test(context,is_onload){
     var city = 'shenzhen';//可修改
     var url = 'http://hotel.qunar.com/city/'+city+'/dt-';
+    var near_hotel = url;
     var hotel = {};
 
     var win = context.contentWindow;
     var match = context.src.match(/dt-\d+/);
     var hotel_id = city + '_' + match[0].substring(3);
     var redirect_id = match[0].substring(3);
+
+    var flag = 0;
+    var random = 0;
+    var type = '';
+
+    //周边酒店
+    if(win.$('.e-recommend-list').length > 0){
+        random = randomNum(1,win.$('.e-recommend-list').length);
+        flag = 0;
+        win.$('.e-recommend-list').each(function () {
+            flag++;
+            if(random == flag){
+                console.log('民宿');
+                type = '民宿';
+                near_hotel = $(this).find('.e-panel-body dl dt a').attr('href');
+            }
+        });
+    }
+    if(win.$('.js_rem_hotel_show').length > 0){
+        random = randomNum(1,win.$('.js_rem_hotel_show').length);
+        flag = 0;
+        win.$('.js_rem_hotel_show').each(function () {
+            flag++;
+            if(random == flag){
+                console.log('酒店');
+                type = '酒店';
+                near_hotel = $(this).find('.txt h4 a').attr('href');
+            }
+        });
+    }
     hotel.id = hotel_id;
     if(!is_onload){
         console.log('onload!');
         if(win.$('.noHotelInfoContainer').length > 0){
             console.log('酒店不存在');
-            $("#qunarFrame").attr("src", url+(++redirect_id)+'/');
+            $("#qunarFrame").attr("src", near_hotel);
             return;
         }
         if(win.$('.waring-icon').length > 0){
             console.log('酒店停业');
-            $("#qunarFrame").attr("src", url+(++redirect_id)+'/');
+            $("#qunarFrame").attr("src", near_hotel);
+            return;
+        }
+        if(win.$('.js_no_com').length > 0){
+            console.log('酒店无评论');
+            $("#qunarFrame").attr("src", near_hotel);
             return;
         }
 
         hotel.name = win.$('.htl-info').find('h2 span').text();
+        if(win.$('.b-baseinfo-title').length > 0){
+            hotel.name = win.$('.b-baseinfo-title').find('h2 span').text();
+        }
 
         var score = win.$('.score');
         if(score.hasClass('no_score')){
@@ -62,18 +115,20 @@ function test(context,is_onload){
 
         $.getJSON('http://localhost/QunarSpider/demo/store_hotel.php?jsoncallback=?',hotel,function(result){
             console.log(result);
+            if(result.res == '失败'){
+                $("#qunarFrame").attr("src", near_hotel);
+            }
         });
     }
 
 
-    if(win.$('.js_no_com').length > 0){
-        console.log('酒店无评论');
-        $("#qunarFrame").attr("src", url+(++redirect_id)+'/');
-        return;
-    }
+
     var data = {};
     var length = win.$('.b_ugcfeed').length;
-    console.log('aa:'+length);
+    console.log('当前页面评论数:'+length);
+    if(length == 0){
+        $("#qunarFrame").attr("src", near_hotel);
+    }
 
     var count = 0;
     win.$('.b_ugcfeed').map(function(index, value){
@@ -131,10 +186,13 @@ function test(context,is_onload){
                     }
                 });
                 if(!next){
-                    $("#qunarFrame").attr("src", url+(++redirect_id)+'/');
+                    console.log('没有下一页了，正在跳转周边酒店');
+                    if(type == '民宿'){
+                        $("#qunarFrame").attr("src", url+(++redirect_id)+'/');
+                    }else{
+                        $("#qunarFrame").attr("src", near_hotel);
+                    }
                 }
-            }else {
-                console.log('nonono');
             }
         });
     });
